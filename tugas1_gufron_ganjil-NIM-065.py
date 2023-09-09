097359c5c41eceb636c4f81490255c2d54eba1b3
gfron17 <43749946+Bagus-id@users.noreply.github.com>
2023-09-09 15:11:08 +0700

    Add files via upload

---
+++ tugas1_gufron_ganjil-NIM-065.py
@@ -0,0 +1,19 @@
+# mengkonversi_Suhu_Celsius_Ke_Fahrenhait
+
+c = int (input("masukkan nilai suhu c yang anda konversi ke suhu f = "))
+conversi_cel = 9*c/5+32
+
+print ('.............................................')
+print (f'hasil konversi {c} C menjadi {conversi_cel}F')
+print ('.............................................')
+
+
+
+# mengkonversi_Suhu_Fahrenhit_Ke_Celsius
+
+k = int (input("masukkan nilai suhu K yang anda konversi ke suhu c ="))
+konvversi_kelvin = k - 273
+
+print ('..................................................')
+print (f'hasil konversi {k} K menjadi {konvversi_kelvin} C')
+print ('..................................................')
